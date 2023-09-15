5b7dba3fbf9b136e36522c99dd00d5c552bdcc65
mveiskats <mveiskats@gocardless.com>
2023-09-15 11:04:02 +0300

    Top-words solution

---
+++ w1-most-frequently-used-words-in-a-text/mveiskats.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+def each_word(io)
+  io.scan(/[0-9a-zA-Z']+/).each { |word| yield word }
+end
+
+def top_3_words(input)
+  occurences = {}
+
+  each_word(input) do |word|
+    occurences[word] ||= 0
+    occurences[word] += 1
+  end
+
+  occurences.to_a
+    .sort_by { |_word, count| -count }
+    .first(3)
+    .map { |word, _count| word }
+end
+
+input = ARGF.read()
+puts top_3_words(input).join(", ")
