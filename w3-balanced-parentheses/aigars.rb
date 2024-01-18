66c30915cf62a5e7cb8d805bc9aae4e7bd63e546
Aigars Šķiliņš <askilins@gocardless.com>
2024-01-18 19:16:05 +0200

    naive implementation

---
+++ w3-balanced-parentheses/aigars.rb
@@ -0,0 +1,21 @@
+# good till about 13
+# not efficient!!
+
+def all_ways_to_balance n
+  raise "Number of parentheses can't have fractions!" unless n.is_a? Integer
+  return [""] if n < 1
+
+  insiders(n).uniq
+end
+
+def insiders n, perm = "", list = []
+  if n == 0
+    list << perm
+    return
+  end
+  insiders(n - 1, "(#{perm})", list)
+  insiders(n - 1, "#{perm}()", list)
+  insiders(n - 1, "()#{perm}", list)
+  list 
+end
+
