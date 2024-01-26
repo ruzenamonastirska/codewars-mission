5385935050c5981c8e3518c22d4b0e3e8fa6a279
azavalnijs-gc <azavalnijs@gocardless.com>
2024-01-26 12:13:13 +0200

    upd for azavalnijs for balans parenthesis

--- w3-balanced-parentheses/azavalnijs-gc.rb
+++ w3-balanced-parentheses/azavalnijs-gc.rb
@@ -6,3 +6,14 @@ def balanced_parens(n, opened=0, closed=0, str="", result=[])
 
   result
 end
+
+# more elegant if you are a code golfer
+def bp(n, opened=0, closed=0, str="")
+  return str if closed == n
+
+  [ *(bp(n, opened + 1, closed, str + "(") if opened < n),
+    *(bp(n, opened, closed + 1, str + ")") if closed < opened) ]
+end
+
+p balanced_parens(3)
+p bp(3)
