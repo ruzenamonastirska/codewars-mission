d1b95e602d01872c0479df54e2a3e81dabae265c
Mikus <melksnitis@gocardless.com>
2024-01-26 10:49:45 +0200

    clean up

--- w3-balanced-parentheses/mikus.rb
+++ w3-balanced-parentheses/mikus.rb
@@ -2,11 +2,11 @@ def balanced_parens(n)
   n == 0 ? [""] : generate_parentheses(n, n).flatten.compact
 end
 
-def generate_parentheses(open, close, current = "", result = [])
-  return current if close == 0
+def generate_parentheses(open, close, current = "")
+  return current  if  close == 0
 
   [
-    (generate_parentheses(open - 1, close, current + "(", result) if open > 0),
-    (generate_parentheses(open, close - 1, current + ")", result) if close > open)
+    (generate_parentheses(open - 1, close, current + "(") if open > 0),
+    (generate_parentheses(open, close - 1, current + ")") if close > open)
   ]
 end
