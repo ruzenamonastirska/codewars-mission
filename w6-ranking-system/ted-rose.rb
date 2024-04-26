4fdbe1ac071324aa4830c804099620b29f31bdd9
Tedis Rozenfelds <trozenfelds@gocardless.com>
2024-04-26 21:10:57 +0300

    Done in 30 lines

--- w6-ranking-system/ted-rose.rb
+++ w6-ranking-system/ted-rose.rb
@@ -1,5 +1,4 @@
 class User
-  attr_reader :rank
   attr_accessor :progress, :valid_ranks
 
   def initialize
@@ -8,47 +7,48 @@ class User
     @progress = 0
   end
 
-  def rank=(value)
-    raise ArgumentError, 'Rank cannot be set to 0' unless @valid_ranks.include?(value)
+  def set_or_get_rank(value = nil)
+    raise ArgumentError unless @valid_ranks.include?(value) || value.nil?
 
-    @rank = value
+    value.nil? ? @rank : @rank = value
   end
 
   def evaluate_rank(user = self)
     if @progress >= 100
       @progress = [0, @progress - 100].max
-      (user.rank = (@rank != -1 ? [8, @rank + 1].min : 1))
+      user.set_or_get_rank(@rank != -1 ? [8, @rank + 1].min : 1)
     end
-
-    @progress = 0 if @rank == 8
-    evaluate_rank if @progress > 99
+    evaluate_rank if @progress > 99 || ((@progress = 0) if @rank == 8)
   end
 
   def inc_progress(activity)
-    diff = (activity.positive? ? activity - 1 : activity) - (@rank.positive? ? @rank - 1 : @rank)
+    diff = (activity.positive? ? activity - 1 : activity) - (@rank > 0 ? @rank - 1 : @rank)
     (@progress += 1) && evaluate_rank if diff == -1 && (@rank != 8)
     (@progress += 3) && evaluate_rank if diff.zero? && (@rank != 8)
-    (@progress += 10 * (diff**2)) && evaluate_rank && (@rank != 8)
+    (@progress += 10 * (diff**2)) && evaluate_rank && (@rank != 8) if diff > -1
   end
 end
 
 def test(user, activity_rank)
   puts("\nactivity_rank = #{activity_rank}\nUser stats after activity accomplished:")
   user.inc_progress(activity_rank)
-  puts("player_rank = #{user.rank}", "progress = #{user.progress}")
+  puts("player_rank = #{user.set_or_get_rank}", "progress = #{user.progress}")
 end
 
 Ted = User.new
 
+puts("-------------------\nTest inc_progress:")
 Ted.valid_ranks.each do |activity_rank|
   test(Ted, activity_rank)
 end
 
+puts("\n\n-------------------\nTest too low activity:")
 test(Ted, 3)
 
+puts("\n\n-------------------\nTest invalid rank defined:")
 begin
-  bad_rank_user = Ted.rank = 10
-  puts("\nAssigned bad_rank_user.rank = #{bad_rank_user.rank}!")
+  bad_rank_user = Ted.set_or_get_rank(10)
+  puts("\nAssigned bad_rank_user.rank = #{bad_rank_user.set_or_get_rank}!")
 rescue ArgumentError
   puts("\nInvalid rank defined !")
 end
