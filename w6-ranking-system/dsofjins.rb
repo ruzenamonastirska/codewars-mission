391d8ec6ad5e32c221220753fb8eebdbb24dc9dc
Danny D. Sofjins <97402971+Sofjins@users.noreply.github.com>
2024-04-29 14:47:13 +0300

    Rating puzzle dsofjins

--- w6-ranking-system/dsofjins.rb
+++ w6-ranking-system/dsofjins.rb
@@ -1,39 +1,51 @@
 class User
-  attr_accessor :current_rank, :current_progress, :added_progress
+  attr_accessor :rank, :progress
 
   def initialize
-    @current_rank = -8
-    @added_progress = 0
-    @current_progress = 0
-  end
-
-  def rank
-    puts "Users rank is #{@current_rank}"
-  end
-
-  def progress
-    @added_progress += @current_progress
-    while @added_progress > 99 && @current_rank < 9
-      if @current_rank == -1
-        @current_rank += 2
-        @added_progress -= 100
-      else
-        @current_rank += 1
-        @added_progress -= 100
-      end
-    end
+    @rank = -8
+    @progress = 0
   end
 
   def inc_progress (puzzle_rank)
-    if puzzle_rank < @current_rank
-      diff = @current_rank - puzzle_rank
-      @current_progress = 10 * diff * diff
-    elsif puzzle_rank > @current_rank
-      diff = puzzle_rank - @current_rank
-      counted = 10 * diff * diff
-      @current_progress += counted
+    raise ArgumentError unless (1..8).include?(puzzle_rank.abs)
+
+
+    if puzzle_rank < self.rank
+      diff = puzzle_rank - self.rank
     else
-      puts "Please don't input 0!"
+      diff = (puzzle_rank - self.rank).abs
+    end
+    if puzzle_rank * self.rank < 0 && self.rank < puzzle_rank
+      diff -= 1
+    end
+    if diff == 0
+      self.progress += 3
+    elsif diff == -1
+      self.progress += 1
+    elsif diff < -1
+      if self.rank > 0
+        self.progress += 1
+      else
+        self.progress == 0
+      end
+    else
+      self.progress += (10 * diff * diff)
+    end
+
+    while self.progress > 99 && self.rank != 0
+      if self.rank == -1
+        self.rank += 2
+        self.progress -= 100
+      else
+        self.rank += 1
+        self.progress -= 100
+      end
+    end
+    if self.rank == 8
+      self.progress = 0
     end
   end
 end
+
+
+
