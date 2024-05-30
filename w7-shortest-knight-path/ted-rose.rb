81cffce2aacfe98db5f4773d61ee7b490a683334
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-05-30 15:58:53 +0300

    Finds only 1 path

--- w7-shortest-knight-path/ted-rose.rb
+++ w7-shortest-knight-path/ted-rose.rb
@@ -13,25 +13,46 @@ def move_knight(path, finish)
   possible_paths = []
   start_letter = path[-1][0].ord - 'a'.ord + 1
   start_number = path[-1][1].to_i
+  puts("start_letter: ", start_letter)
+  puts("start_number: ", start_number)
 
   POSSIBLE_MOVES.each_value do |letter, number|
     possible_path = path
     new_letter = start_letter + letter
     new_number = start_number + number
-    next unless (new_letter.positive? && new_letter < 9) && (new_number.positive? && new_number < 9)
+    puts("\nnew_letter: ", new_letter)
+    puts("new_number: ", new_number)
+    puts("possible_paths:", possible_paths.inspect)
+
+    if !new_letter.positive? or new_letter > 8 or !new_number.positive? or new_number > 8
+      puts("Can't move outside the board!")
+      next
+    end
 
     new_position = (new_letter + 'a'.ord - 1).chr + new_number.to_s
-    next if possible_path.include?(new_position)
+    puts("new_position: ", new_position)
+    if possible_path.include?(new_position)
+      puts("Don't repeat your moves!")
+      next
+    end
 
-    puts("\nnew_position in move_knight:", new_position)
+    puts("new_position in move_knight:", new_position)
     possible_path << new_position
-    puts("current path: #{path.inspect}")
+    puts("current path: #{possible_path.inspect}")
+
     if new_position == finish
       puts("Brought knight home!")
-      possible_paths << path
+      possible_paths << possible_path
       break
     else
-      possible_paths << move_knight(possible_path, finish)
+      puts("Looking for more possible_path's")
+      some_possible_paths = move_knight(possible_path, finish)
+
+      unless some_possible_paths == []
+        puts("some_possible_paths wasn't empty:", some_possible_paths.inspect)
+        possible_paths << some_possible_paths
+      end
+
     end
   end
   possible_paths
