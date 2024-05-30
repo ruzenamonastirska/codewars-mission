c2a88fe77fb68842ade77f73599167d926f6ea2d
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-05-30 15:27:39 +0300

    This does something with less code

--- w7-shortest-knight-path/ted-rose.rb
+++ w7-shortest-knight-path/ted-rose.rb
@@ -1,87 +1,63 @@
-
+POSSIBLE_MOVES = {
+  'up_left' => [-1, 2],
+  'up_right' => [1, 2],
+  'right_up' => [2, 1],
+  'down_left' => [-1, 2],
+  'down_right' => [1, 2],
+  'right_down' => [2, -1],
+  'left_up' => [2, 1],
+  'left_down' => [2, -1],
+}
 
 def move_knight(path, finish)
-  possible_moves = {
-    'up_left' => [-1, 2],
-    'up_right' => [1, 2],
-    'right_up' => [2, 1],
-    'down_left' => [-1, 2],
-    'down_right' => [1, 2],
-    'right_down' => [2, -1],
-    'left_up' => [2, 1],
-    'left_down' => [2, -1],
-  }
   possible_paths = []
-
-
   start_letter = path[-1][0].ord - 'a'.ord + 1
   start_number = path[-1][1].to_i
 
-  puts(start_letter)
-  puts(start_number)
-
-  possible_moves.each_value do |letter, number|
+  POSSIBLE_MOVES.each_value do |letter, number|
+    possible_path = path
     new_letter = start_letter + letter
     new_number = start_number + number
     next unless (new_letter.positive? && new_letter < 9) && (new_number.positive? && new_number < 9)
 
     new_position = (new_letter + 'a'.ord - 1).chr + new_number.to_s
+    next if possible_path.include?(new_position)
+
     puts("\nnew_position in move_knight:", new_position)
-    path << new_position
+    possible_path << new_position
     puts("current path: #{path.inspect}")
     if new_position == finish
       puts("Brought knight home!")
       possible_paths << path
-      next
+      break
+    else
+      possible_paths << move_knight(possible_path, finish)
     end
   end
   possible_paths
 end
 
-def shortest_knight_path(start, finish)
-  possible_moves = {
-    'up_left' => [-1, 2],
-    'up_right' => [1, 2],
-    'right_up' => [2, 1],
-    'down_left' => [-1, 2],
-    'down_right' => [1, 2],
-    'right_down' => [2, -1],
-    'left_up' => [2, 1],
-    'left_down' => [2, -1],
-  }
-
-  possible_paths = []
-  start_letter = start[0].ord - 'a'.ord + 1
-  start_number = start[1].to_i
-  finish_letter = finish[0].ord - 'a'.ord + 1
-  finish_number = finish[1].to_i
-
-  puts(start_letter)
-  puts(start_number)
-  puts(finish_letter)
-  puts(finish_number, "\n")
-
-  possible_moves.each_value do |letter, number|
-    new_letter = start_letter + letter
-    new_number = start_number + number
-    next unless (new_letter.positive? && new_letter < 9) && (new_number.positive? && new_number < 9)
-
-    new_position = (new_letter + 'a'.ord - 1).chr + new_number.to_s
-    puts("\nnew_position in main function: ", new_position)
-    path = [start, new_position]
-    if new_position == finish
-      possible_paths << path
-      next
+def remove_empty_arrays(array)
+  array.each_with_object([]) do |element, result|
+    if element.is_a?(Array)
+      cleaned_element = remove_empty_arrays(element)
+      result << cleaned_element unless cleaned_element.empty?
     else
-      some_possible_paths = move_knight(path, finish)
-      possible_paths << some_possible_paths
-      puts("Got this path from move_knight:\n#{possible_paths.inspect}")
+      result << element
     end
+  end
+end
+
+def shortest_knight_path(start, finish)
+    single_square_path = [start]
+    possible_paths = move_knight(single_square_path, finish)
+    # possible_paths << possible_paths
+    # puts("Got this path from move_knight:\n#{possible_paths.inspect}")
+
     puts("possible_paths:\n#{possible_paths.inspect}")
-    non_empty_arrays = possible_paths.flatten(1).reject(&:empty?)
+    non_empty_arrays = remove_empty_arrays(possible_paths)
     non_empty_arrays.min_by(&:length)
     puts("Shortest path:\n#{non_empty_arrays.min_by(&:length).inspect}")
 
-  end
 end
-shortest_knight_path('a3', 'd4')
+shortest_knight_path('a3', 'b4')
