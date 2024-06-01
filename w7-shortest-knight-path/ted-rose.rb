aaf7da038c86d4378cff77c02c667f200e8aabe4
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-06-01 17:46:55 +0300

    OOP draft

--- w7-shortest-knight-path/ted-rose.rb
+++ w7-shortest-knight-path/ted-rose.rb
@@ -10,51 +10,58 @@ POSSIBLE_MOVES = {
 }
 POSSIBLE_PATHS = []
 
-def move_knight(path, finish)
-  current_letter = path[-1][0].ord - 'a'.ord + 1
-  current_number = path[-1][1].to_i
-  print("current_letter value: ", current_letter, "; ")
-  print("current_number value: ", current_number, "\n")
+class ChessMoves
+  def initialize(path)
+    @path = path
+  end
 
-  POSSIBLE_MOVES.each_value do |letter, number|
-    current_thread_id = Thread.current.object_id
-    puts "Current Thread ID: #{current_thread_id}"
-    possible_path = path
-    new_letter = current_letter + letter
-    new_number = current_number + number
-    print("\nPOSSIBLE_PATHS:", POSSIBLE_PATHS.inspect, "\n")
-    new_position = (new_letter + 'a'.ord - 1).chr + new_number.to_s
-    print("new_position: ", new_position, " by adding letter '", letter, "' and '", number, "'\n")
+  def move_knight()
+    current_letter = @path[-1][0].ord - 'a'.ord + 1
+    current_number = @path[-1][1].to_i
+    print("current_letter value: ", current_letter, "; ")
+    print("current_number value: ", current_number, "\n")
 
-    if !new_letter.positive? or new_letter > 8 or !new_number.positive? or new_number > 8
-      puts("Can't move outside the board!")
-      next
-    end
+    POSSIBLE_MOVES.each_value do |letter, number|
+      current_thread_id = Thread.current.object_id
+      puts "Current Thread ID: #{current_thread_id}"
+      possible_path = @path
+      new_letter = current_letter + letter
+      new_number = current_number + number
+      print("\nPOSSIBLE_PATHS:", POSSIBLE_PATHS.inspect, "\n")
+      new_position = (new_letter + 'a'.ord - 1).chr + new_number.to_s
+      print("new_position: ", new_position, " by adding letter '", letter, "' and '", number, "'\n")
 
-    if possible_path.include?(new_position)
-      puts("Don't repeat your moves!")
-      next
-    end
-
-    possible_path << new_position
-    print("current path: #{possible_path.inspect}\n")
-
-    if new_position == finish
-      puts("Brought knight home!")
-      POSSIBLE_PATHS << possible_path
-      break
-    else
-      puts("Looking for more possible_path's")
-      some_possible_paths = move_knight(possible_path, finish)
-
-      unless some_possible_paths == []
-        puts("some_possible_paths wasn't empty:", some_possible_paths.inspect)
-        POSSIBLE_PATHS << some_possible_paths
+      if !new_letter.positive? or new_letter > 8 or !new_number.positive? or new_number > 8
+        puts("Can't move outside the board!")
+        next
       end
 
+      if possible_path.include?(new_position)
+        puts("Don't repeat your moves!")
+        next
+      end
+
+      possible_path << new_position
+      print("current path: #{possible_path.inspect}\n")
+
+      if new_position == $THE_FINISH
+        puts("Brought knight home!")
+        POSSIBLE_PATHS << possible_path
+        break
+      else
+        puts("Looking for more possible_path's")
+        chess_moves = ChessMoves.new(possible_path)
+        some_possible_paths = self.move_knight()
+
+        unless some_possible_paths == []
+          puts("some_possible_paths wasn't empty:", some_possible_paths.inspect)
+          POSSIBLE_PATHS << some_possible_paths
+        end
+
+      end
     end
+    POSSIBLE_PATHS
   end
-  POSSIBLE_PATHS
 end
 
 def remove_empty_arrays(array)
@@ -70,7 +77,9 @@ end
 
 def shortest_knight_path(start, finish)
     single_square_path = [start]
-    possible_paths = move_knight(single_square_path, finish)
+    $THE_FINISH = finish
+    chess_moves = ChessMoves.new(single_square_path)
+    possible_paths = chess_moves.move_knight()
     # possible_paths << possible_paths
     # puts("Got this path from move_knight:\n#{possible_paths.inspect}")
 
