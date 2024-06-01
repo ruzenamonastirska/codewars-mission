0e6663ba722de9ef23218576d9be859464cae658
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-06-01 16:44:45 +0300

    Why only 1 path found?

--- w7-shortest-knight-path/ted-rose.rb
+++ w7-shortest-knight-path/ted-rose.rb
@@ -11,34 +11,31 @@ POSSIBLE_MOVES = {
 
 def move_knight(path, finish)
   possible_paths = []
-  start_letter = path[-1][0].ord - 'a'.ord + 1
-  start_number = path[-1][1].to_i
-  puts("start_letter: ", start_letter)
-  puts("start_number: ", start_number)
+  current_letter = path[-1][0].ord - 'a'.ord + 1
+  current_number = path[-1][1].to_i
+  print("current_letter value: ", current_letter, "; ")
+  print("current_number value: ", current_number)
 
   POSSIBLE_MOVES.each_value do |letter, number|
     possible_path = path
-    new_letter = start_letter + letter
-    new_number = start_number + number
-    puts("\nnew_letter: ", new_letter)
-    puts("new_number: ", new_number)
-    puts("possible_paths:", possible_paths.inspect)
+    new_letter = current_letter + letter
+    new_number = current_number + number
+    print("\npossible_paths:", possible_paths.inspect, "\n")
+    new_position = (new_letter + 'a'.ord - 1).chr + new_number.to_s
+    print("new_position: ", new_position, " by adding letter '", letter, "' and '", number, "'\n")
 
     if !new_letter.positive? or new_letter > 8 or !new_number.positive? or new_number > 8
       puts("Can't move outside the board!")
       next
     end
 
-    new_position = (new_letter + 'a'.ord - 1).chr + new_number.to_s
-    puts("new_position: ", new_position)
     if possible_path.include?(new_position)
       puts("Don't repeat your moves!")
       next
     end
 
-    puts("new_position in move_knight:", new_position)
     possible_path << new_position
-    puts("current path: #{possible_path.inspect}")
+    print("current path: #{possible_path.inspect}\n")
 
     if new_position == finish
       puts("Brought knight home!")
