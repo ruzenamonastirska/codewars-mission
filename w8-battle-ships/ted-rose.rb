d6b29212172a84ef1259837205d2f057f18122ba
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-06-17 17:54:47 +0300

    Passed first board

--- w8-battle-ships/ted-rose.rb
+++ w8-battle-ships/ted-rose.rb
@@ -1,25 +1,48 @@
+def find_ships(board)
+  ships = [
+    [],
+    [],
+    [],
+  ]
+  puts("ships:", ships.inspect)
+  board.each_with_index do |row, row_number|
+    print("\n\nrow is:", row)
+    puts("row_number is:", row_number)
+    row.each_with_index do |ship_number, column_number|
+      puts("\nship_number:", ship_number)
+      if ship_number.zero?
+        puts("No ship on field [#{column_number}, #{row_number}]")
+        next
+      end
+
+      ships[ship_number - 1] << [column_number + 1, row_number + 1]
+    end
+  end
+  puts("ships:", ships.inspect)
+  ships
+end
+
 def damaged_or_sunk(board, attacks)
-  ships = {
-    'first' => [[3, 1], [3, 2], [2, 3]],
-    'second' => [],
-    'third' => [],
-  }
-  results = {'sunk'=> 0, 'damaged' => 0, 'not_touched' => 0, 'points' => 0 }
+  ships = find_ships(board)
+  results = {'sunk' => 0, 'damaged' => 0, 'not_touched' => 0, 'points' => 0 }
 
-  ships.each do |name, coordinates|
-    puts("There is no #{name} ship!") && next if coordinates.empty?
-
-    not_touched = -1
+  ships.each_with_index do |coordinates, i|
+    puts("Ship coordinates:", coordinates.inspect)
     damaged = 0
+    next if coordinates.empty?
+    not_touched = -1
+
     attacks.each do |attack|
       next unless coordinates.include?(attack)
 
       coordinates.delete(attack)
+      puts("Deleted coordinates")
       damaged += 0.5
       not_touched = 0
     end
-
-    if ships[name].empty?
+    puts("coordinates aftert deleting:", coordinates.inspect)
+    if coordinates.empty?
+      puts("Points because empty")
       results['sunk'] += 1
       results['points'] += results['sunk']
     else
@@ -30,7 +53,10 @@ def damaged_or_sunk(board, attacks)
     results['points'] += results['not_touched']
     end
   end
+  puts("results", results)
+  results
 end
+
 
 # codewars tests
 board = [ [0, 0, 1, 0],
