9c99abda9259597e5c7e1d897f8e2c12901c7731
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-06-19 16:43:15 +0300

    Passed all tests

--- w8-battle-ships/ted-rose.rb
+++ w8-battle-ships/ted-rose.rb
@@ -4,10 +4,13 @@ def find_ships(board)
     [],
     [],
   ]
-  puts("ships:", ships.inspect)
-  board.each_with_index do |row, row_number|
-    print("\n\nrow is:", row)
-    puts("row_number is:", row_number)
+  board_lines = board.length
+  board.each_with_index do |row, row_index|
+    puts("\n\nrow is:", row.inspect)
+    puts("row_index is:", row_index)
+    # Row numbering starts from the most bottom line
+    row_number = board_lines - row_index
+
     row.each_with_index do |ship_number, column_number|
       puts("\nship_number:", ship_number)
       if ship_number.zero?
@@ -15,7 +18,7 @@ def find_ships(board)
         next
       end
 
-      ships[ship_number - 1] << [column_number + 1, row_number + 1]
+      ships[ship_number - 1] << [column_number + 1, row_number]
     end
   end
   puts("ships:", ships.inspect)
@@ -27,30 +30,37 @@ def damaged_or_sunk(board, attacks)
   results = {'sunk' => 0, 'damaged' => 0, 'not_touched' => 0, 'points' => 0 }
 
   ships.each_with_index do |coordinates, i|
-    puts("Ship coordinates:", coordinates.inspect)
+    puts("\nShip coordinates:", coordinates.inspect)
     damaged = 0
     next if coordinates.empty?
-    not_touched = -1
+    not_touched = 0
 
     attacks.each do |attack|
       next unless coordinates.include?(attack)
 
       coordinates.delete(attack)
       puts("Deleted coordinates")
-      damaged += 0.5
-      not_touched = 0
+      damaged = 0.5
+      puts("damaged: #{damaged}")
     end
     puts("coordinates aftert deleting:", coordinates.inspect)
+
     if coordinates.empty?
       puts("Points because empty")
       results['sunk'] += 1
-      results['points'] += results['sunk']
-    else
-      results['damaged'] += damaged
-      results['points'] += results['damaged']
+      results['points'] += 1
+    elsif damaged.nonzero?
+      puts("Points because not empty")
+      results['damaged'] += 1
+      results['points'] += damaged
+    elsif damaged.zero?
+      not_touched = 1
+      results['points'] += - 1
 
+    puts("damaged at end: #{damaged}")
+    puts("not_touched: #{not_touched}")
     results['not_touched'] += not_touched
-    results['points'] += results['not_touched']
+
     end
   end
   puts("results", results)
@@ -58,14 +68,14 @@ def damaged_or_sunk(board, attacks)
 end
 
 
-# codewars tests
-board = [ [0, 0, 1, 0],
-          [0, 0, 1, 0],
-          [0, 0, 1, 0] ]
-
-attacks = [[3, 1], [3, 2], [3, 3]];
-result = damaged_or_sunk(board, attacks)
-puts"Game 1 result: { 'sunk': 1, 'damaged': 0 , 'not_touched': 0, 'points': 1}\n\n"
+# # codewars tests
+# board = [ [0, 0, 1, 0],
+#           [0, 0, 1, 0],
+#           [0, 0, 1, 0] ]
+#
+# attacks = [[3, 1], [3, 2], [3, 3]];
+# result = damaged_or_sunk(board, attacks)
+# puts"Game 1 result: { 'sunk': 1, 'damaged': 0 , 'not_touched': 0, 'points': 1}\n\n"
 
 board = [ [3, 0, 1],
           [3, 0, 1],
